b925a3c78ef052ed82c71bbce4f7da5585494b49
Jared Farrish <jf@dialogs.com>
2020-07-27 13:26:40 -0500

    Server script updates

--- web/wp-content/object-cache.php
+++ web/wp-content/object-cache.php
@@ -3,25 +3,27 @@
 global $redis_server;
 
 if ($_ENV['REDIS_URL']) {
-    $connection = parse_url($_ENV['REDIS_URL']);
-
-    $connection['path'] = trim($connection['path'] ?? '', '/') ?: $_ENV['WEB_HOSTNAME'];
+    if (file_exists(WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php')) {
+        $connection = parse_url($_ENV['REDIS_URL']);
     
-    if (!defined('WP_CACHE_KEY_SALT') && $connection['path']) {
-        define('WP_CACHE_KEY_SALT', $connection['path']);
+        $connection['path'] = trim($connection['path'] ?? '', '/') ?: $_ENV['WEB_HOSTNAME'];
+    
+        if (!defined('WP_CACHE_KEY_SALT') && $connection['path']) {
+            define('WP_CACHE_KEY_SALT', $connection['path']);
+        }
+    
+        $protocol = $_ENV['DEPLOY_ENV'] === 'local' ? '' : 'tls://';
+    
+        $redis_server = array(
+            'host'     => $protocol.$connection['host'],
+            'port'     => $connection['port'] ?? 6379,
+            'database' => 0,
+        );
+    
+        if ($connection['pass'] ?? '') {
+            $redis_server['auth'] = $connection['pass'] ?? '';
+        }
+        
+        require_once WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php';
     }
-    
-    $protocol = $_ENV['DEPLOY_ENV'] === 'local' ? '' : 'tls://';
-    
-    $redis_server = array(
-        'host'     => $protocol.$connection['host'],
-        'port'     => $connection['port'] ?? 6379,
-        'database' => 0,
-    );
-    
-    if ($connection['pass'] ?? '') {
-        $redis_server['auth'] = $connection['pass'] ?? '';
-    }
-
-    require_once WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php';
 }
